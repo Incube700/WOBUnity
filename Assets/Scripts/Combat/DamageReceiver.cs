6633c7b6e0624ee5e9b76827e0562f93ba855bb5
BurnHeartGames <Belkajor@gmail.com>
2026-04-27 00:51:31 +0530

    Fix DamageReceiver class name for legacy scripts

--- Assets/Scripts/Combat/DamageReceiver.cs
+++ Assets/Scripts/Combat/DamageReceiver.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 /// <summary>
 /// Abstract base class for any component that wants to receive damage from bullets.
+/// Legacy compatibility base for old prototype scripts.
 /// </summary>
-public abstract class DamageReceiver2D : MonoBehaviour
+public abstract class DamageReceiver : MonoBehaviour
 {
     public abstract void ApplyDamage(float amount);
 }
